3cdcc670dad7bf896ee08d2c792c97910ae968ea
BYJU VP <BYJU VP@BYJU>
2023-11-15 11:45:22 +0530

    Logout pushed

--- src/main/java/gethub/DummyForGetHub.java
+++ src/main/java/gethub/DummyForGetHub.java
@@ -5,6 +5,7 @@ public class DummyForGetHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("login done");
+System.out.println("logout done");
 
 }
 }
